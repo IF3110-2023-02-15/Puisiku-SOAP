b28704279dcd5375b503cec0ced43c7d29ace60a
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-25 15:52:52 +0700

    feat: init subscription interface

---
+++ src/main/java/com/chagiya/services/interfaces/SubscriptionService.java
@@ -0,0 +1,16 @@
+package com.chagiya.services.interfaces;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService(targetNamespace = "http://services.chagiya.com/")
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
+public interface SubscriptionService {
+    @WebMethod
+    String subscribe(@WebParam(name = "email") String email);
+
+    @WebMethod
+    String checkSubscription(@WebParam(name = "email") String email);
+}
