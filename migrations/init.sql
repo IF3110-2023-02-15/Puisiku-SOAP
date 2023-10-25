92bba2f4a3ded57cd12b164549eab107a75d50df
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-25 15:40:15 +0700

    feat: init db migration

---
+++ migrations/init.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS logs;
+
+CREATE TABLE IF NOT EXISTS logs (
+    id                  INT             AUTO_INCREMENT PRIMARY KEY,
+    description         TEXT            NOT NULL,
+    ip_address          VARCHAR(15)     NOT NULL,
+    endpoint            VARCHAR(255)    NOT NULL,
+    requester           VARCHAR(50)     NOT NULL,
+    timestamp           TIMESTAMP       DEFAULT CURRENT_TIMESTAMP
+);
