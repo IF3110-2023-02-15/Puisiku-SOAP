30caaa7db68e75d624069f9772f0f60b89311e3a
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-25 15:56:45 +0700

    refactor: decompose function and remove magic number

--- src/main/java/com/chagiya/main/Main.java
+++ src/main/java/com/chagiya/main/Main.java
@@ -1,14 +1,21 @@
 package com.chagiya.main;
 
-import com.chagiya.services.LoggingService;
+import com.chagiya.services.implementors.SubscriptionServiceImpl;
 
 import javax.xml.ws.Endpoint;
 
 public class Main {
+    private static final String HOST_IP = "0.0.0.0";
+    private static final String PORT = "8888";
+
     public static void main(String[] args) {
+        publishEndpoint("/subscription", new SubscriptionServiceImpl());
+    }
+
+    private static void publishEndpoint(String path, Object service) {
         try {
-            Endpoint.publish("http://0.0.0.0:8888/logging", new LoggingService());
-            System.out.println("SOAP service serve and list on localhost:8888");
+            Endpoint.publish("http://" + HOST_IP + ":" + PORT + path, service);
+            System.out.println("SOAP service serve and list on " + HOST_IP + ":" + PORT + path);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
