04e21b546dbd17bde4f4bd74ae660abf2fa82462
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-12 01:42:20 +0700

    chore: use response model and do try catch

--- src/main/java/com/chagiya/services/implementors/SubscriptionServiceImpl.java
+++ src/main/java/com/chagiya/services/implementors/SubscriptionServiceImpl.java
@@ -1,8 +1,10 @@
 package com.chagiya.services.implementors;
 
 import com.chagiya.models.SubscriptionModel;
+import com.chagiya.models.response.Response;
 import com.chagiya.repositories.LoggingRepository;
 import com.chagiya.repositories.SubscriptionRepository;
+import com.chagiya.services.interfaces.LoggingService;
 import com.chagiya.services.interfaces.SubscriptionService;
 
 import javax.jws.WebParam;
@@ -12,24 +14,26 @@ import javax.jws.soap.SOAPBinding;
 @WebService(targetNamespace = "http://services.chagiya.com/")
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
 public class SubscriptionServiceImpl implements SubscriptionService {
-    private final LoggingServiceImpl ls;
-    public SubscriptionServiceImpl() {
-        ls = new LoggingServiceImpl();
+    private final SubscriptionRepository subscriptionRepository = new SubscriptionRepository();
+    private final LoggingService loggingService = new LoggingServiceImpl();
+
+    @Override
+    public Response subscribe(@WebParam(name = "email") String email, @WebParam(name = "creatorId") int creatorId) {
+        SubscriptionModel subscriptionModel = new SubscriptionModel(email, creatorId);
+
+        loggingService.log(email);
+
+        try {
+            subscriptionRepository.subscribe(subscriptionModel);
+            return new Response(200, email + " subscribed to creator with id " + creatorId);
+        } catch(Exception e) {
+            return new Response(400, e.toString());
+        }
     }
 
     @Override
-    public String subscribe(@WebParam(name = "email") String email) {
-        SubscriptionModel sm = new SubscriptionModel(email);
-
-        SubscriptionRepository sr = new SubscriptionRepository();
-        sr.addLogging(sm);
-        ls.log(email);
-        
-        return "Email " + email + " has successfully subscribed";
-    }
-
-    @Override
-    public String checkSubscription(@WebParam(name = "email") String email) {
-        return "Email " + email + " has a subscription!";
+    public Response getSubscriberCount(@WebParam(name = "creatorId") int creatorId) {
+        Integer subscriberCount = subscriptionRepository.getSubscriberCount(creatorId);
+        return new Response(200, subscriberCount.toString());
     }
 }
